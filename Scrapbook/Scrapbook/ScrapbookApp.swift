e392ade2e192bfd42179ad1732c4365433ec8568
taehyeonk <xogus9641@naver.com>
2022-04-28 21:32:21 +0900

    add: 프로젝트 생성

---
+++ Scrapbook/Scrapbook/ScrapbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrapbookApp.swift
+//  Scrapbook
+//
+//  Created by Admin on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct ScrapbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
