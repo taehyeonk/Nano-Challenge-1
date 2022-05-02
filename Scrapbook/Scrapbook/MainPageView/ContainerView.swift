bb5bf1348157db46774847c61f4fdfe837904eb2
taehyeonk <xogus9641@naver.com>
2022-05-02 21:39:39 +0900

    fix: bookmark 카드 내 글자 패딩 및 정렬, 이미지 크롭 문제 수정
    - bookmark 제목과 요약문 사이에 알 수 없는 패딩이 생기는 문제 수정
    - bookmark 요약문 길이가 짧아질 경우 왼쪽 정렬 되지 않는 문제 수정
    - 세로가 긴 이미지 크롭 시 공백이 생기는 부분 수정

--- Scrapbook/Scrapbook/MainPageView/ContainerView.swift
+++ Scrapbook/Scrapbook/MainPageView/ContainerView.swift
@@ -32,85 +32,95 @@ struct BookmarksView: View {
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 20) {
                         NavigationLink(destination: WebView(urlToLoad: "https://www.youtube.com/")) {
-                            VStack {
-                                Image("ios_01")
+                            VStack(alignment: .center, spacing: 0) {
+                                Image("design_02")
                                     .resizable()
                                     .aspectRatio(contentMode: .fill)
-                                VStack(alignment: .leading) {
-                                    Text("개발 지침서 목록 모음")
-                                        .font(.system(.title3, design: .rounded))
-                                        .fontWeight(.bold)
-                                        .foregroundColor(.black)
-                                        .padding(.horizontal, 10)
-                                    Text("iOS 개발을 위해서 꼭 알아야 할 필수 덕목 10가지 가나다라마바사 아자차카타파하 아러니ㅏㅇ러미나어리망너리망너림아너림ㄴ아ㅓ림ㄴ아ㅓ림ㄴ아러ㅣㅁㄴ아ㅓ린아러민아러미낭러")
-                                        .font(.system(.subheadline, design: .rounded))
-                                        .fontWeight(.medium)
-                                        .foregroundColor(.gray)
-                                        .multilineTextAlignment(.leading)
-                                        .lineLimit(3)
-                                        .padding(5)
-                                        .padding(.horizontal, 10)
-                                        .padding(.bottom, 10)
+                                    .frame(height: 200)
+                                    .clipped()
+                                HStack {
+                                    VStack(alignment: .leading, spacing: 0) {
+                                        Text("개발 지침서 목록 모음")
+                                            .font(.system(.title3, design: .rounded))
+                                            .fontWeight(.bold)
+                                            .foregroundColor(.black)
+                                            .padding(.bottom, 5)
+                                        Text("iOS 개발을 위한 필수 덕목 10가지")
+                                            .font(.system(.subheadline, design: .rounded))
+                                            .fontWeight(.medium)
+                                            .foregroundColor(.gray)
+                                            .multilineTextAlignment(.leading)
+                                            .lineLimit(3)
+                                    }
+                                    .padding(10)
+                                    Spacer(minLength: 0)
                                 }
-                                .frame(height: 100)
+                                Spacer()
+
                             }
-                            .frame(width: 280, height: 320)
+                            .frame(width: 280, height: 310)
                             .background(Color.white)
                             .cornerRadius(30)
                         }
                         
                         NavigationLink(destination: WebView(urlToLoad: "https://www.naver.com/")) {
-                            VStack {
-                                Image("ios_02")
+                            VStack(alignment: .center, spacing: 0) {
+                                Image("ios_01")
                                     .resizable()
                                     .aspectRatio(contentMode: .fill)
-                                VStack(alignment: .leading) {
-                                    Text("좋은 개발자로 성장하기 위한 방법")
-                                        .font(.system(.title3, design: .rounded))
-                                        .fontWeight(.bold)
-                                        .foregroundColor(.black)
-                                        .padding(.horizontal, 10)
-                                    Text("잘 설명할 수 없는 경험은 지원서에 없느니만 못하다는 것입니다.\n지원서에 작성한 내용은 당연히 질문 리스트에 포함이 되고, 충분한 답변을 기대하게 됩니다.\n그리고 질문하는 사람은 현업에서 상당한 경험을 쌓은 실력자들입니다.\n그러니 충분히 대답할 수 없는 것들, 자신이 크게 기여하지 않은 부분들은 적지 않는 것이 좋습니다.\n오히려 어떤 부분에서 어떤 역할을 했는지 뚜렷이 적는 것이 도움이 됩니다.")
-                                        .font(.system(.subheadline, design: .rounded))
-                                        .fontWeight(.medium)
-                                        .foregroundColor(.gray)
-                                        .multilineTextAlignment(.leading)
-                                        .lineLimit(3)
-                                        .padding(.top, 5)
-                                        .padding(.leading, 10)
-                                        .padding(.bottom, 15)
+                                    .frame(height: 200)
+                                    .clipped()
+                                HStack {
+                                    VStack(alignment: .leading, spacing: 0) {
+                                        Text("좋은 개발자로 성장하기 위한 방법")
+                                            .font(.system(.title3, design: .rounded))
+                                            .fontWeight(.bold)
+                                            .foregroundColor(.black)
+                                            .padding(.bottom, 5)
+                                        Text("잘 설명할 수 없는 경험은 지원서에 없느니만 못하다는 것입니다.\n지원서에 작성한 내용은 당연히 질문 리스트에 포함이 되고, 충분한 답변을 기대하게 됩니다.\n그리고 질문하는 사람은 현업에서 상당한 경험을 쌓은 실력자들입니다.\n그러니 충분히 대답할 수 없는 것들, 자신이 크게 기여하지 않은 부분들은 적지 않는 것이 좋습니다.\n오히려 어떤 부분에서 어떤 역할을 했는지 뚜렷이 적는 것이 도움이 됩니다.")
+                                            .font(.system(.subheadline, design: .rounded))
+                                            .fontWeight(.medium)
+                                            .foregroundColor(.gray)
+                                            .multilineTextAlignment(.leading)
+                                            .lineLimit(3)
+                                    }
+                                    .padding(10)
+                                    Spacer(minLength: 0)
                                 }
-                                .frame(height: 100)
+                                Spacer(minLength: 0)
                             }
-                            .frame(width: 280, height: 320)
+                            .frame(width: 280, height: 310)
                             .background(Color.white)
-                            .cornerRadius(30)
+                            .cornerRadius(20)
                         }
                         
                         NavigationLink(destination: WebView(urlToLoad: "https://www.google.com/")) {
-                            VStack {
+                            VStack(alignment: .center, spacing: 0) {
                                 Image("ios_03")
                                     .resizable()
                                     .aspectRatio(contentMode: .fill)
-                                VStack(alignment: .leading) {
-                                    Text("좋은 개발자로 성장하기 위한 방법")
-                                        .font(.system(.title3, design: .rounded))
-                                        .fontWeight(.bold)
-                                        .foregroundColor(.black)
-                                        .padding(.horizontal, 10)
-                                    Text("잘 설명할 수 없는 경험은 지원서에 없느니만 못하다는 것입니다.\n지원서에 작성한 내용은 당연히 질문 리스트에 포함이 되고, 충분한 답변을 기대하게 됩니다.\n그리고 질문하는 사람은 현업에서 상당한 경험을 쌓은 실력자들입니다.\n그러니 충분히 대답할 수 없는 것들, 자신이 크게 기여하지 않은 부분들은 적지 않는 것이 좋습니다.\n오히려 어떤 부분에서 어떤 역할을 했는지 뚜렷이 적는 것이 도움이 됩니다.")
-                                        .font(.system(.subheadline, design: .rounded))
-                                        .fontWeight(.medium)
-                                        .foregroundColor(.gray)
-                                        .multilineTextAlignment(.leading)
-                                        .lineLimit(3)
-                                        .padding(.top, 5)
-                                        .padding(.leading, 10)
-                                        .padding(.bottom, 15)
+                                    .frame(height: 200)
+                                    .clipped()
+                                HStack {
+                                    VStack(alignment: .leading, spacing: 0) {
+                                        Text("좋은 개발자로 성장하기 위한 방법")
+                                            .font(.system(.title3, design: .rounded))
+                                            .fontWeight(.bold)
+                                            .foregroundColor(.black)
+                                            .padding(.bottom, 5)
+                                        Text("잘 설명할 수 없는 경험은 지원서에 없느니만 못하다는 것입니다.\n지원서에 작성한 내용은 당연히 질문 리스트에 포함이 되고, 충분한 답변을 기대하게 됩니다.\n그리고 질문하는 사람은 현업에서 상당한 경험을 쌓은 실력자들입니다.\n그러니 충분히 대답할 수 없는 것들, 자신이 크게 기여하지 않은 부분들은 적지 않는 것이 좋습니다.\n오히려 어떤 부분에서 어떤 역할을 했는지 뚜렷이 적는 것이 도움이 됩니다.")
+                                            .font(.system(.subheadline, design: .rounded))
+                                            .fontWeight(.medium)
+                                            .foregroundColor(.gray)
+                                            .multilineTextAlignment(.leading)
+                                            .lineLimit(3)
+                                    }
+                                    .padding(10)
+                                    Spacer(minLength: 0)
                                 }
-                                .frame(height: 100)
+                                Spacer(minLength: 0)
                             }
-                            .frame(width: 280, height: 320)
+                            .frame(width: 280, height: 300)
                             .background(Color.white)
                             .cornerRadius(30)
                         }
