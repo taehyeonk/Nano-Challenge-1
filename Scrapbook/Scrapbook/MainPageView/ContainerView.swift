e59f95180881eafd1251d1fc324ddda3011f27c0
taehyeonk <xogus9641@naver.com>
2022-05-03 17:04:33 +0900

    feat: ContainerView json파일과 연결하는 부분 구현

--- Scrapbook/Scrapbook/MainPageView/ContainerView.swift
+++ Scrapbook/Scrapbook/MainPageView/ContainerView.swift
@@ -9,106 +9,44 @@ import SwiftUI
 
 struct ContainerView: View {
     var body: some View {
-        VStack(spacing: 30) {
-            BookmarksView()
-            BookmarksView()
+        VStack(spacing: 20) {
+            ForEach(categories, id: \.id) {category in
+                BookmarksView(cateogry: category)
+            }
         }
-        
     }
 }
 
 struct BookmarksView: View {
+    var cateogry: Category
+    
     var body: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                Text("iOS 개발")
-                    .font(.system(.title2, design: .rounded))
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
-                    .padding(.horizontal, 30)
-                
-                
+        VStack(alignment: .leading) {
+            Text(cateogry.name)
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+                .padding(.horizontal, 30)
 
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 20) {
-                        NavigationLink(destination: WebView(urlToLoad: "https://www.youtube.com/")) {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    ForEach(cateogry.bookmarks, id: \.id) { bookmark in
+                        NavigationLink(destination: WebView(urlToLoad: bookmark.url)) {
                             VStack(alignment: .center, spacing: 0) {
-                                Image("design_02")
+                                bookmark.image
                                     .resizable()
                                     .aspectRatio(contentMode: .fill)
                                     .frame(height: 200)
                                     .clipped()
                                 HStack {
                                     VStack(alignment: .leading, spacing: 0) {
-                                        Text("개발 지침서 목록 모음")
+                                        Text(bookmark.title)
                                             .font(.system(.title3, design: .rounded))
                                             .fontWeight(.bold)
                                             .foregroundColor(.black)
                                             .padding(.bottom, 5)
-                                        Text("iOS 개발을 위한 필수 덕목 10가지")
-                                            .font(.system(.subheadline, design: .rounded))
-                                            .fontWeight(.medium)
-                                            .foregroundColor(.gray)
-                                            .multilineTextAlignment(.leading)
-                                            .lineLimit(2)
-                                    }
-                                    .padding(10)
-                                    Spacer(minLength: 0)
-                                }
-                                Spacer()
-
-                            }
-                            .frame(width: 280, height: 300)
-                            .background(Color.white)
-                            .cornerRadius(20)
-                        }
-                        
-                        NavigationLink(destination: WebView(urlToLoad: "https://www.naver.com/")) {
-                            VStack(alignment: .center, spacing: 0) {
-                                Image("ios_01")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(height: 200)
-                                    .clipped()
-                                HStack {
-                                    VStack(alignment: .leading, spacing: 0) {
-                                        Text("좋은 개발자로 성장하기 위한 방법")
-                                            .font(.system(.title3, design: .rounded))
-                                            .fontWeight(.bold)
-                                            .foregroundColor(.black)
-                                            .padding(.bottom, 5)
-                                        Text("잘 설명할 수 없는 경험은 지원서에 없느니만 못하다는 것입니다.\n지원서에 작성한 내용은 당연히 질문 리스트에 포함이 되고, 충분한 답변을 기대하게 됩니다.\n그리고 질문하는 사람은 현업에서 상당한 경험을 쌓은 실력자들입니다.\n그러니 충분히 대답할 수 없는 것들, 자신이 크게 기여하지 않은 부분들은 적지 않는 것이 좋습니다.\n오히려 어떤 부분에서 어떤 역할을 했는지 뚜렷이 적는 것이 도움이 됩니다.")
-                                            .font(.system(.subheadline, design: .rounded))
-                                            .fontWeight(.medium)
-                                            .foregroundColor(.gray)
-                                            .multilineTextAlignment(.leading)
-                                            .lineLimit(2)
-                                    }
-                                    .padding(10)
-                                    Spacer(minLength: 0)
-                                }
-                                Spacer(minLength: 0)
-                            }
-                            .frame(width: 280, height: 300)
-                            .background(Color.white)
-                            .cornerRadius(20)
-                        }
-                        
-                        NavigationLink(destination: WebView(urlToLoad: "https://www.google.com/")) {
-                            VStack(alignment: .center, spacing: 0) {
-                                Image("ios_03")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(height: 200)
-                                    .clipped()
-                                HStack {
-                                    VStack(alignment: .leading, spacing: 0) {
-                                        Text("좋은 개발자로 성장하기 위한 방법")
-                                            .font(.system(.title3, design: .rounded))
-                                            .fontWeight(.bold)
-                                            .foregroundColor(.black)
-                                            .padding(.bottom, 5)
-                                        Text("잘 설명할 수 없는 경험은 지원서에 없느니만 못하다는 것입니다.\n지원서에 작성한 내용은 당연히 질문 리스트에 포함이 되고, 충분한 답변을 기대하게 됩니다.\n그리고 질문하는 사람은 현업에서 상당한 경험을 쌓은 실력자들입니다.\n그러니 충분히 대답할 수 없는 것들, 자신이 크게 기여하지 않은 부분들은 적지 않는 것이 좋습니다.\n오히려 어떤 부분에서 어떤 역할을 했는지 뚜렷이 적는 것이 도움이 됩니다.")
+                                            .lineLimit(1)
+                                        Text(bookmark.description)
                                             .font(.system(.subheadline, design: .rounded))
                                             .fontWeight(.medium)
                                             .foregroundColor(.gray)
@@ -125,9 +63,8 @@ struct BookmarksView: View {
                             .cornerRadius(20)
                         }
                     }
-                    .padding(.horizontal, 20)
                 }
-                
+                .padding(.horizontal, 20)
             }
         }
     }
